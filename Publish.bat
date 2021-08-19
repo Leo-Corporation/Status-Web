0907f37962b4e1c71ee320cd31974a80db5d5406
Léo Peyronnet <leo.peyronnet@outlook.fr>
2021-08-19 10:30:32 +0200

    Updated Status Page

---
+++ Publish.bat
@@ -0,0 +1,4 @@
+git add .
+git commit -m "Updated Status Page"
+git push
+echo "Published"
--- Refresh.bat
+++ Refresh.bat
@@ -1,7 +1,2 @@
 cd src/statusfy
 npm run generate
-cd ../..
-git add .
-git commit -m "Updated Status Page"
-git push
-pause
