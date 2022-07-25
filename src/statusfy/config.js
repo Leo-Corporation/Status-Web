d28ef8806ebc4e7c826853326bfae1e8100d1f9c
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-07-25 09:34:34 +0200

    Update config.js

--- src/statusfy/config.js
+++ src/statusfy/config.js
@@ -37,12 +37,5 @@ module.exports = {
     twitter: {
       en: "LeoCorpNews"
     }
-  },
-  head: {
-    link: [
-      { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap' }
-    ]
   }
 }
