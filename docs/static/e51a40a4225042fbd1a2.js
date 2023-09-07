b66911b9454e86a75ff37cb2133a7bbbda091ac8
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-09-07 07:14:36 +0200

    Updated Status

--- docs/static/e51a40a4225042fbd1a2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{270:function(r,t,n){"use strict";n.r(t);var o={layout:"404",components:{ErrorPage:n(20).a},data:function(){return{error:{statusCode:404}}}},e=n(9),component=Object(e.a)(o,(function(){return(0,this._self._c)("ErrorPage",{attrs:{error:this.error}})}),[],!1,null,null,null);t.default=component.exports}}]);
