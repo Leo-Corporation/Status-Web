a4f87676f1f21d85a50e42d91528994a53ba52b0
Léo Peyronnet <leo.peyronnet@outlook.fr>
2023-12-09 10:22:17 +0100

    Updated Status Page

---
+++ docs/static/546f4a75ff060e2eddbc.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{272:function(n,e,t){"use strict";t.r(e);var c={fetch:function(n){var e=n.app;(0,n.redirect)(301,e.localePath("index"))}},l=t(9),component=Object(l.a)(c,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
